13f567af8a4ac0807f6b528fd0ec01c81d90ce07
ChillyOctopus <jistevenson01@gmail.com>
2024-05-30 14:46:52 -0600

    Push in case of emergency.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,27 @@ using System.Diagnostics;
 using Elastic.Clients.Elasticsearch;
 using Elastic.Clients.Elasticsearch.Core.Search;
 
+public class MeiRequest{
+    public MeiRequest(string v){
+        meiChunk = v;
+    }
+    public string meiChunk { get; set; }
+}
+
+public class Record
+{
+    public string Name { get; set; }
+    public string IntervalsText { get; set; }
+    public List<int> IntervalsVector { get; set; }
+}
+
+public class Response
+{
+    public List<Hit<Record>> Hits { get; set; }
+    public string Message { get; set; }
+    public bool Success { get; set; }
+}
+
 namespace melodySearchProject.Controllers
 {
     public class HomeController : Controller
@@ -48,43 +69,56 @@ namespace melodySearchProject.Controllers
             MeiRequest mei = new MeiRequest(inputData);
             var jsonINpout = JsonSerializer.Serialize(mei);
 
-            try{
-                using (var client = new HttpClient()){
+            try
+            {
+                using (var client = new HttpClient())
+                {
                     var content = new StringContent(jsonINpout, System.Text.Encoding.UTF8, "application/json");
 
                     HttpResponseMessage response = await client.PostAsync(javaServerUrl, content);
-                    if (response.IsSuccessStatusCode){
-
-                        //First get the response data as a string then deserialize it
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // First get the response data as a string then deserialize it
                         string responseData = await response.Content.ReadAsStringAsync();
-                        Response responseObj;
                         List<string> names = new List<string>();
 
-                        //if it isn't null, deserailize and add all the ids to a list to display
-                        if(responseData != null){
-                            responseObj = JsonSerializer.Deserialize<Response>(responseData);
-                            //If the deserialized isn't null, then add all the names to a list
-                            if(responseObj != null){
-                                foreach(Hit<Record> h in responseObj.hits){names.Add(h.Id);}
-                            } else {
-                                string objNull = "Obj was null!\n";
+                        // Deserialize response data
+                        if (!string.IsNullOrEmpty(responseData))
+                        {
+                            var responseObj = JsonSerializer.Deserialize<Response>(responseData);
+                            // If the deserialized object isn't null, then add all the names to a list
+                            if (responseObj != null)
+                            {
+                                foreach (var hit in responseObj.Hits)
+                                {
+                                    names.Add(hit.Id);
+                                }
+                            }
+                            else
+                            {
+                                string objNull = "Deserialized object was null!\n";
                                 Console.WriteLine(objNull);
                                 Debug.WriteLine(objNull);
                                 return Json(objNull);
                             }
-                        } else {
-                            string responseNull = "Resonse data was null!\n";
+                        }
+                        else
+                        {
+                            string responseNull = "Response data was null!\n";
                             Console.WriteLine(responseNull);
                             Debug.WriteLine(responseNull); 
                             return Json(responseNull);
                         }
+
                         // Redirect to a new action with the response data as a query parameter
-                        string listToString = "";
-                        foreach(string s in names){
-                            listToString = listToString + s + "\n";
-                        }
-                        return RedirectToAction("DisplayResponse", listToString);
-                    } else {
+                        string listToString = string.Join("\n", names);
+                        Console.WriteLine(listToString);
+                        Debug.WriteLine(listToString);  
+                        return Json(names);
+                        //return RedirectToAction("DisplayResponse", new { responseData = listToString });
+                    }
+                    else
+                    {
                         return Json("Error occurred while sending data to Java server.");
                     }
                 }
@@ -96,8 +130,6 @@ namespace melodySearchProject.Controllers
             }
         }
 
-
-
         public ActionResult DisplayResponse(string responseData)
         {
             // Pass the response data to the view
@@ -107,22 +139,7 @@ namespace melodySearchProject.Controllers
 
     }
 
-        public class MeiRequest{
-            public MeiRequest(string v){
-                meiChunk = v;
-            }
-            public string meiChunk { get; set; }
-        }
 
-        public class Record{
-            public string name { get; set; }
-            public string intervals_text { get; set; }
-            public int[] intervals_vector { get; set; }
-        }
-
-        public class Response{
-            public Hit<Record>[] hits { get; set; }
-        }
 };
 
 
